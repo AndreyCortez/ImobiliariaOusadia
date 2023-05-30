85fb677fbdc54962079e5dd8c133e514000c97f8
Mateus S <mssantos456@gmail.com>
2023-05-30 09:03:33 -0300

    Create the component Footer and add a homepage

---
+++ front-imobiliariaousadia/src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaTwitter, FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="left-text">Since 2023</div>
+      <div className="right-icons">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://linkedin.com">
+          <FaLinkedin />
+        </a>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ front-imobiliariaousadia/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer/Footer.js';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>Put your content here...</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
