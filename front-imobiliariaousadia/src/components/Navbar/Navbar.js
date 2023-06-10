7578a844f547611dd894e621a857c7e6bd18adad
Mateus S <mssantos456@gmail.com>
2023-06-10 12:06:25 -0300

    fix: navbar responsive

--- front-imobiliariaousadia/src/components/Navbar/Navbar.js
+++ front-imobiliariaousadia/src/components/Navbar/Navbar.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import companyLogo from '../../images/CompanyLogo.png';
-import {NavLink} from "react-router-dom"
+import { NavLink } from 'react-router-dom';
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <nav className="navbar">
       <img src={companyLogo} alt="Company Logo" className="logo" />
       <div className="title">Ousadia Imobiliária</div>
+
+      <div className={`menu-icon ${showMenu ? 'open' : ''}`} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
 
       <ul className={`nav-NavLinks ${showMenu ? 'show' : ''}`}>
         <li><NavLink to="/">Home</NavLink></li>
