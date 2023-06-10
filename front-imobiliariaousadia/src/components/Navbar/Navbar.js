1bb64d0c1e2002e0ab1727d38a24d3edee5e7d19
Mateus S <mssantos456@gmail.com>
2023-06-10 11:56:02 -0300

    edições no navbar

--- front-imobiliariaousadia/src/components/Navbar/Navbar.js
+++ front-imobiliariaousadia/src/components/Navbar/Navbar.js
@@ -3,23 +3,16 @@ import './Navbar.css';
 import companyLogo from '../../images/CompanyLogo.png';
 import {NavLink} from "react-router-dom"
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
 
   return (
     <nav className="navbar">
       <img src={companyLogo} alt="Company Logo" className="logo" />
       <div className="title">Ousadia Imobiliária</div>
-      <button className="menu-icon" onClick={toggleMenu}>
-        <i className={showMenu ? 'fas fa-times' : 'fas fa-bars'}></i>
-      </button>
+
       <ul className={`nav-NavLinks ${showMenu ? 'show' : ''}`}>
         <li><NavLink to="/">Home</NavLink></li>
         <li><a href="/">Sell</a></li>
-        <li><a href="/">About Us</a></li>
+        <li><NavLink to="/aboutus">About Us</NavLink></li>
         <li><a href="/">Buy</a></li>
         <li><a href="/">Agents</a></li>
         <li><a href="/">Sign In</a></li>
